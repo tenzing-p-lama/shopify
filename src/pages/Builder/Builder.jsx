a612c07a3f4d0463a2674b2bc5b811178a2e4308
Tenzing P Lama <tenzingpalden520@gmail.com>
2023-11-17 02:07:03 -0800

    Builder page complete

--- src/pages/Builder/Builder.jsx
+++ src/pages/Builder/Builder.jsx
@@ -1,8 +1,9 @@
 import "./Builder.scss";
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import inperson from "../../assets/icons/image 3.png";
 import online from "../../assets/icons/image 4.png";
+import ShadowBox from "../../components/ShadowBox/ShadowBox";
 
 function Builder() {
   const navigate = useNavigate();
@@ -13,46 +14,86 @@ function Builder() {
 
   return (
     <div className="shopdetails">
-      <form className="shopdetails-form" onSubmit={handleFormSubmit}>
-        <h2 className="shopdetails__header">Shop Details</h2>
-        <p>To get started, input some information about your shop</p>
-
-        <input type="text" name="name" id="name" placeholder="Shop name" />
-        <input type="text" name="url" id="url" placeholder="Custom URL" />
-        <textarea
-          name="description"
-          id="description"
-          cols="30"
-          rows="5"
-          placeholder="Shop Description"
-        ></textarea>
-
-        <h2>Where would you like to sell?</h2>
-
-        <div className="shopdetails-form__sell">
-          <section>
-            <div className="shopdetails-form__platform">
-              <img src={inperson} alt="store icon" />
-              <h3>In-Person</h3>
-              <p>Selling at a physical Location</p>
+      <ShadowBox>
+        <form className="shopdetails-form" onSubmit={handleFormSubmit}>
+          <div className="shopdetails__content">
+            <h1 className="shopdetails__header">Shop Details</h1>
+            <div className="shopdetails__description">
+              <p>To get started, input some information about your shop</p>
             </div>
-            <div className="shopdetails-form__platform">
-              <img src={online} alt="online icon" />
-              <h3>Online</h3>
-              <p>Create a fully customizable Website</p>
-            </div>
-          </section>
-        </div>
+          </div>
 
-        <h2>What website would you like to reference</h2>
+          <div className="shopdetails__content">
+            <input
+              className="form__input"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Shop name"
+            />
+            <input
+              className="form__input"
+              type="text"
+              name="url"
+              id="url"
+              placeholder="Custom URL"
+            />
+            <textarea
+              className="form__textarea"
+              name="description"
+              id="description"
+              cols="30"
+              rows="5"
+              placeholder="Shop Description"
+            ></textarea>
+          </div>
 
-        <input
-          type="text"
-          name="reference"
-          id="reference"
-          placeholder="https://"
-        />
-      </form>
+          <div className="shopdetails__content">
+            <h1 className="shopdetails__header">
+              Where would you like to sell?
+            </h1>
+
+            <section className="shopdetails-form__sell">
+              <div className="shopdetails-form__platform">
+                <img
+                  className="shopdetails-form__content-img"
+                  src={inperson}
+                  alt="store icon"
+                />
+                <h3 className="shopdetails-form__content">In-Person</h3>
+                <p> Selling at a physical Location</p>
+              </div>
+
+              <div className="shopdetails-form__platform">
+                <img
+                  className="shopdetails-form__content-img"
+                  src={online}
+                  alt="online icon"
+                />
+                <h3 className="shopdetails-form__content">Online</h3>
+                <p>Create a fully customizable Website</p>
+              </div>
+            </section>
+          </div>
+
+          <div className="shopdetails__content">
+            <h1 className="shopdetails__header">
+              What website would you like to reference
+            </h1>
+            <input
+              className="form__input"
+              type="text"
+              name="reference"
+              id="reference"
+              placeholder="https://"
+            />
+          </div>
+
+          <div className="shopdetails__button-container">
+            <button className="shopdetails__button">Generate a Website</button>
+          </div>
+        </form>
+      </ShadowBox>
     </div>
   );
 }
