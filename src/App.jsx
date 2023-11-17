10219999fcee760449a1bb19902464b29c96b657
Tenzing P Lama <tenzingpalden520@gmail.com>
2023-11-16 22:13:18 -0800

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div className="App">
-      <p>hello shopify</p>
+    <div>
+      <Header />
     </div>
   );
 }
