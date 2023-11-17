e4583dc6f1839968777ea002dcf44f345004c420
Mostafa <mosti.asghari@gmail.com>
2023-11-16 22:20:15 -0800

    Style SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.scss";
 import { Link } from "react-router-dom";
 import homeIcon from "../../assets/Home.png";
@@ -19,88 +19,148 @@ import searchDiscoveryIcon from "../../assets/SearchDiscovery.png";
 import flowIcon from "../../assets/Flow.png";
 
 function SideBar() {
+  const [selected, setSelected] = useState("home");
+  const getListItemClass = (itemName) => {
+    return selected === itemName
+      ? "sidebar__list-item--selected"
+      : "sidebar__list-item";
+  };
+
   return (
     <nav className="sidebar">
-      <div>
+      <div className="sidebar__list-container">
         <ul className="sidebar__list">
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("home")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("home")}
+            >
               <img src={homeIcon} alt="home logo" />
               Home
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("order")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("order")}
+            >
               <img src={orderIcon} alt="Orders logo" />
               Orders
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("products")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("products")}
+            >
               <img src={productsIcon} alt="products logo" />
               Products
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("customers")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("customers")}
+            >
               <img src={customersIcon} alt="Customers logo" />
               Customers
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("content")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("content")}
+            >
               <img src={contentIcon} alt="Content logo" />
               Content
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("finances")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("finances")}
+            >
               <img src={financesIcon} alt="Finances logo" />
               Finances
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("analytics")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("analytics")}
+            >
               <img src={analyticsIcon} alt="Analytics logo" />
               Analytics
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("marketing")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("marketing")}
+            >
               <img src={marketingIcon} alt="Marketing logo" />
               Marketing
             </Link>
           </li>
-          <li className="sidebar__list-item">
-            <Link to={"/"} className="sidebar__list-link">
+          <li className={getListItemClass("discounts")}>
+            <Link
+              to={"/"}
+              className="sidebar__list-link"
+              onClick={() => setSelected("discounts")}
+            >
               <img src={discountIcon} alt="Discounts logo" />
               Discounts
             </Link>
           </li>
         </ul>
 
-        <div className="sidebar__list">
+        <div className="sidebar__list-section">
           <div className="sidebar__list-label">
-            <Link to="/" className="sidebar__list-link">
+            <Link
+              to="/"
+              className="sidebar__list-label-link"
+              onClick={() => setSelected("salesChannels")}
+            >
               Sales channels
+              <i class="fa-solid fa-chevron-right"></i>
             </Link>
           </div>
 
-          <ul>
-            <li className="sidebar__list-item">
-              <Link to="/" className="sidebar__list-link">
+          <ul className="sidebar__list">
+            <li className={getListItemClass("os")}>
+              <Link
+                to="/"
+                className="sidebar__list-link"
+                onClick={() => setSelected("os")}
+              >
                 <img src={onlineStoreIcon} alt="Online store logo" />
                 Online Store
               </Link>
             </li>
-            <li className="sidebar__list-item">
-              <Link to="/" className="sidebar__list-link">
+            <li className={getListItemClass("pos")}>
+              <Link
+                to="/"
+                className="sidebar__list-link"
+                onClick={() => setSelected("pos")}
+              >
                 <img src={posIcon} alt="point of sale logo" />
                 Point of Sale
               </Link>
             </li>
-            <li className="sidebar__list-item">
-              <Link to="/" className="sidebar__list-link">
+            <li className={getListItemClass("shop")}>
+              <Link
+                to="/"
+                className="sidebar__list-link"
+                onClick={() => setSelected("shop")}
+              >
                 <img src={shopIcon} alt="shop logo" />
                 Shop
               </Link>
@@ -108,22 +168,35 @@ function SideBar() {
           </ul>
         </div>
 
-        <div className="sidebar__list">
+        <div className="sidebar__list-section">
           <div className="sidebar__list-label">
-            <Link to="/" className="sidebar__list-link">
+            <Link
+              to="/"
+              className="sidebar__list-label-link"
+              onClick={() => setSelected("apps")}
+            >
               Apps
+              <i class="fa-solid fa-chevron-right"></i>
             </Link>
           </div>
 
-          <ul>
-            <li className="sidebar__list-item">
-              <Link to="/" className="sidebar__list-link">
+          <ul className="sidebar__list">
+            <li className={getListItemClass("email")}>
+              <Link
+                to="/"
+                className="sidebar__list-link"
+                onClick={() => setSelected("email")}
+              >
                 <img src={emailIcon} alt="email logo" />
                 Shopify Email
               </Link>
             </li>
-            <li className="sidebar__list-item">
-              <Link to="/" className="sidebar__list-link">
+            <li className={getListItemClass("ssd")}>
+              <Link
+                to="/"
+                className="sidebar__list-link"
+                onClick={() => setSelected("ssd")}
+              >
                 <img
                   src={searchDiscoveryIcon}
                   alt="Shopify Search & Discovery logo"
@@ -131,8 +204,12 @@ function SideBar() {
                 Shopify Search & Discovery
               </Link>
             </li>
-            <li className="sidebar__list-item">
-              <Link to="/" className="sidebar__list-link">
+            <li className={getListItemClass("flow")}>
+              <Link
+                to="/"
+                className="sidebar__list-link"
+                onClick={() => setSelected("flow")}
+              >
                 <img src={flowIcon} alt="Flow logo" />
                 Shopify Flow
               </Link>
@@ -140,8 +217,12 @@ function SideBar() {
           </ul>
         </div>
       </div>
-      <div className="sidebar__list-label">
-        <Link to="/" className="sidebar__list-link">
+      <div className="sidebar__list-label--setting">
+        <Link
+          to="/"
+          className="sidebar__list-link"
+          onClick={() => setSelected("setting")}
+        >
           <img src={settingsIcon} alt="setting" />
           Settings
         </Link>
