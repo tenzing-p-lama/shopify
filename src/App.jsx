3ed10dcd48e1d26010a3685258c35f22353dc63e
Mostafa <mosti.asghari@gmail.com>
2023-11-16 22:25:09 -0800

    Implement layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
 import Home from "./pages/Home/Home";
+import "./App.scss";
+
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <SideBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <div>Header</div>
+        <main>
+          <SideBar />
+          <div className="app__content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </main>
       </BrowserRouter>
     </div>
   );
